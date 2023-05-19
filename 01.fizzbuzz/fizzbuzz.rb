b587395f3a4e59b169b95cb08c48d7267197df51
matsuyama <aya.k.matsuyama@gmail.com>
2023-05-19 19:34:20 +0900

    add fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,12 @@
+n = 1
+while n <= 20
+  if n % 3 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  else puts n
+  end
+  n += 1
+end
