d26dd4b2f3580fa7ce5522aaad221d001882df65
matsuyama <aya.k.matsuyama@gmail.com>
2023-07-18 07:38:02 +0900

    Make changes to program logic to improve flexibility for future alteration

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,55 +1,70 @@
 # frozen_string_literal: true
 
-def display_contents(dir = '.')
+def display_contents(dir)
   contents = modified_contents(dir)
-  row_count = calculate_rows(contents)
-  divided_contents = slice_contents(contents, row_count)
-  position(row_count, divided_contents)
+  rows = calculate_rows(contents)
+  divided_contents = slice_contents(rows, contents)
+  contents_rearranged = rearrange_contents(rows, divided_contents)
+  display(contents_rearranged)
 end
 
 def modified_contents(dir)
-  # 隠しファイルを排除して、コンテンツを左揃えにするためにパディング
+  # 隠しファイルを排除
   filtered_contents = Dir.children(dir).sort.reject { |content| File.fnmatch('.*', content) }
-  filtered_contents.map do |content|
-    content.ljust(16)
+  # ファイル名の長さに応じてパディングの度合いを変える
+  longest_name = filtered_contents.max_by(&:length)
+  name_count = longest_name.length
+  if name_count >= 16
+    filtered_contents.map do |content|
+      "#{content.ljust(name_count)}    "
+    end
+  else
+    filtered_contents.map do |content|
+      content.ljust(16).to_s
+    end
   end
 end
 
 def calculate_rows(contents)
   # コンテンツを表示するときに必要な行数の算出
   total_contents = contents.size
-  row_count = 4
-  if total_contents <= 12
-    row_count
-  else
-    (total_contents.to_f / 3).ceil
-  end
+  (total_contents.to_f / 3).ceil
 end
 
-def slice_contents(contents, row_count)
+def slice_contents(rows, contents)
   divided_contents = []
-  contents.each_slice(row_count) do |content|
+  contents.each_slice(rows) do |content|
     divided_contents << content
-  end
-  # 次のプロセスに必要な配列の数が足りていない場合追加格納する
-  min_required_arrays = 3
-  (min_required_arrays - divided_contents.size).times do
-    divided_contents << []
   end
   divided_contents
 end
 
-# グリッドを利用してコンテンツの場所を入れ替える
-def position(row_count, divided_contents)
-  grid = Array.new(row_count) { Array.new(3, '') }
-  grid.each_with_index do |row, row_i|
-    row.each_with_index do |_col, col_i|
-      grid[row_i][col_i] = divided_contents[col_i][row_i]
+def rearrange_contents(rows, divided_contents)
+  # 指定された順序で表示できるようにコンテンツの並びを変えて新しく配列にしまう
+  contents_rearranged = []
+  row_count = 0
+  col_count = 0
+  until col_count >= rows
+    divided_contents.size.times do
+      contents_rearranged << divided_contents[row_count][col_count]
+      row_count += 1
+    end
+    row_count = 0
+    col_count += 1
+  end
+  contents_rearranged
+end
+
+def display(contents_rearranged)
+  contents_rearranged.each_with_index do |content, i|
+    print content
+    if contents_rearranged.size == 4
+      puts if (i + 1).even?
+    elsif ((i + 1) % 3).zero?
+      puts
     end
   end
-  grid.each do |row|
-    puts row.join('')
-  end
+  puts
 end
 
 dir = ARGV[0] || '.'
