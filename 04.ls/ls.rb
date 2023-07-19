05707de36a00e8081179bdab1fdc2c955ef7f307
matsuyama <aya.k.matsuyama@gmail.com>
2023-07-20 06:18:04 +0900

    Eliminate unnecessary codes and combine codes to improve readability. Also make changes to properly display contents.

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+COLUMNS = 3
+
 def display_contents(dir)
   contents = modified_contents(dir)
-  rows = calculate_rows(contents)
-  divided_contents = slice_contents(rows, contents)
-  contents_rearranged = rearrange_contents(rows, divided_contents)
+  row = calculate_rows(contents)
+  contents_rearranged = rearrange_contents(row, contents)
   display(contents_rearranged)
 end
 
@@ -12,39 +13,25 @@ def modified_contents(dir)
   # 隠しファイルを排除
   filtered_contents = Dir.children(dir).sort.reject { |content| File.fnmatch('.*', content) }
   # ファイル名の長さに応じてパディングの度合いを変える
-  longest_name = filtered_contents.max_by(&:length)
-  name_count = longest_name.length
-  if name_count >= 16
-    filtered_contents.map do |content|
-      "#{content.ljust(name_count)}    "
-    end
-  else
-    filtered_contents.map do |content|
-      content.ljust(16).to_s
-    end
+  longest = filtered_contents.max_by(&:length).length
+  filtered_contents.map do |content|
+    "#{content.ljust(longest)}    "
   end
 end
 
 def calculate_rows(contents)
   # コンテンツを表示するときに必要な行数の算出
   total_contents = contents.size
-  (total_contents.to_f / 3).ceil
+  (total_contents.to_f / COLUMNS).ceil
 end
 
-def slice_contents(rows, contents)
-  divided_contents = []
-  contents.each_slice(rows) do |content|
-    divided_contents << content
-  end
-  divided_contents
-end
-
-def rearrange_contents(rows, divided_contents)
+def rearrange_contents(row, contents)
+  divided_contents = contents.each_slice(row).to_a
   # 指定された順序で表示できるようにコンテンツの並びを変えて新しく配列にしまう
   contents_rearranged = []
   row_count = 0
   col_count = 0
-  until col_count >= rows
+  until col_count >= row
     divided_contents.size.times do
       contents_rearranged << divided_contents[row_count][col_count]
       row_count += 1
@@ -56,15 +43,24 @@ def rearrange_contents(rows, divided_contents)
 end
 
 def display(contents_rearranged)
-  contents_rearranged.each_with_index do |content, i|
-    print content
-    if contents_rearranged.size == 4
-      puts if (i + 1).even?
-    elsif ((i + 1) % 3).zero?
-      puts
+  contents_count = contents_rearranged.compact.size
+  if contents_count <= COLUMNS
+    puts contents_rearranged.join('')
+    return
+  end
+  # 2列表示される場合の計算
+  # 2列表示に必要な最大コンテンツ数を算出して、適切な場所で改行する
+  if contents_count <= (COLUMNS * 2) - 2
+    contents_rearranged.each_with_index do |content, i|
+      print content
+      puts if i + 1 == (contents_count.to_f / 2).ceil
+    end
+  else
+    contents_rearranged.each_with_index do |content, i|
+      print content
+      puts if ((i + 1) % COLUMNS).zero?
     end
   end
-  puts
 end
 
 dir = ARGV[0] || '.'
