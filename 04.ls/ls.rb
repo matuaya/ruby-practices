40c55f4aba8b19b87e66278ed6fdfab2c7c1f87a
matsuyama <aya.k.matsuyama@gmail.com>
2023-08-14 03:55:20 +0900

    Rename variables and methods, simplify 'file_and_symlinks',refactor 'display_with_information' to improve readability

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -15,62 +15,62 @@ def display_contents(dir, option)
   file_names = file_names(dir, option)
   return display_with_information(file_names, dir) if option[:l]
 
-  contents = format_file_list(file_names)
-  row = calculate_rows(contents)
-  contents_rearranged = rearrange_contents(row, contents)
-  display(contents_rearranged)
+  files = format_file_list(file_names)
+  row = calculate_rows(files)
+  files_rearranged = rearrange_files(row, files)
+  display(files_rearranged)
 end
 
 def file_names(dir, option)
   file_names = Dir.entries(dir).sort
   file_names = file_names.reverse if option[:r]
-  file_names = file_names.reject { |content| File.fnmatch('.*', content) } unless option[:a]
+  file_names = file_names.reject { |file_name| File.fnmatch('.*', file_name) } unless option[:a]
   file_names
 end
 
 def format_file_list(file_names)
   # ファイル名の長さに応じてパディングの度合いを変える
   longest = file_names.max_by(&:length).length
-  file_names.map do |content|
-    "#{content.ljust(longest)}    "
+  file_names.map do |file_name|
+    "#{file_name.ljust(longest)}    "
   end
 end
 
-def calculate_rows(contents)
+def calculate_rows(files)
   # コンテンツを表示するときに必要な行数の算出
-  total_contents = contents.size
-  (total_contents.to_f / COLUMNS).ceil
+  total_files = files.size
+  (total_files.to_f / COLUMNS).ceil
 end
 
-def rearrange_contents(row, contents)
-  divided_contents = contents.each_slice(row).to_a
+def rearrange_files(row, files)
+  divided_files = files.each_slice(row).to_a
   # 指定された順序で表示できるようにコンテンツの並びを変えて新しく配列にしまう
-  contents_rearranged = []
+  files_rearranged = []
   row_count = 0
   col_count = 0
   until col_count >= row
-    divided_contents.size.times do
-      contents_rearranged << divided_contents[row_count][col_count]
+    divided_files.size.times do
+      files_rearranged << divided_files[row_count][col_count]
       row_count += 1
     end
     row_count = 0
     col_count += 1
   end
-  contents_rearranged
+  files_rearranged
 end
 
-def display(contents_rearranged)
-  contents_count = contents_rearranged.compact.size
-  if contents_count <= COLUMNS
-    puts contents_rearranged.join('')
+def display(files_rearranged)
+  files_count = files_rearranged.compact.size
+  if files_count <= COLUMNS
+    puts files_rearranged.join('')
     return
   end
   # 2列表示される場合の計算
   # 2列表示に必要な最大コンテンツ数を算出して、適切な場所で改行する
-  contents_rearranged.each_with_index do |content, i|
-    print content
-    if contents_count <= (COLUMNS * 2) - 2
-      puts if i + 1 == (contents_count.to_f / 2).ceil || i + 1 == contents_rearranged.size
+  files_rearranged.each_with_index do |file, i|
+    print file
+    if files_count <= (COLUMNS * 2) - 2
+      puts if i + 1 == (files_count.to_f / 2).ceil || i + 1 == files_rearranged.size
     else
       puts if ((i + 1) % COLUMNS).zero?
     end
@@ -79,87 +79,74 @@ end
 
 def display_with_information(file_names, dir)
   Dir.chdir(dir)
-  inode_values, lstat_mode_values, nlink_counts, file_size, block_counts = file_attributes(file_names)
-  print_block_count(block_counts)
-
-  file_names.each do |content|
-    lstat_mode = format('%06o', File.lstat(content).mode)
-    print_file_type(lstat_mode)
-    print_permissions(lstat_mode)
-    print_number_of_links(content, nlink_counts)
-    print_names(content)
-    print_contents_size(content, file_size)
-    print_last_accessed_time(content)
-    print_content_names(content, lstat_mode_values, inode_values, lstat_mode)
-  end
-end
-
-def print_block_count(block_counts)
-  total_block_count = block_counts.sum
+  width_size = calculate_width_file_size(file_names)
+  width_nlink = calculate_width_nlink(file_names)
+  total_block_count = file_names.sum { |file_name| File.stat(file_name).blocks }
   puts "total #{total_block_count}"
+
+  file_names.each do |file_name|
+    lstat_mode = format('%06o', File.lstat(file_name).mode)
+    print "#{file_type(lstat_mode)}#{permissions(lstat_mode)}  #{number_of_links(file_name, width_nlink)} "\
+          "#{names(file_name)}  #{file_size(file_name, width_size)} #{last_accessed_time(file_name)} "
+    puts files_and_symlinks(file_name)
+  end
 end
 
-def file_attributes(file_names)
-  inode_values = {}
-  lstat_mode_values = {}
-  nlink_counts = []
+def calculate_width_file_size(file_names)
   file_size = []
-  block_counts = []
-
-  file_names.each do |content|
-    file_information = File.stat(content)
-    inode_values[content] = file_information.ino.to_s
-    lstat_mode_values[content] = File.lstat(content).mode.to_s(8)
-    nlink_counts << file_information.nlink.to_s
-    file_size << file_information.size.to_s
-    block_counts << file_information.blocks
+  file_names.each do |file_name|
+    file_size << File.stat(file_name).size.to_s
   end
-  [inode_values, lstat_mode_values, nlink_counts, file_size, block_counts]
+  file_size.max_by(&:length).length
 end
 
-def print_file_type(lstat_mode)
+def calculate_width_nlink(file_names)
+  nlink_counts = []
+  file_names.each do |file_name|
+    nlink_counts << File.stat(file_name).nlink.to_s
+  end
+  nlink_counts.max_by(&:length).length
+end
+
+def file_type(lstat_mode)
   file_type = lstat_mode.slice(0, 2)
-  print(FILE_TYPE[file_type])
+  FILE_TYPE[file_type]
 end
 
-def print_permissions(lstat_mode)
+def permissions(lstat_mode)
+  permission = ''
   permissions = lstat_mode.slice(-3, 3).chars.to_a
-  permissions.each_with_index do |n, i|
-    print(PERMISSIONS[n])
-    print '  ' if i == 2
+  permissions.each do |n|
+    permission += PERMISSIONS[n]
   end
+  permission
 end
 
-def print_names(content)
-  ids = [File.stat(content).uid, File.stat(content).gid]
-  print "#{Etc.getpwuid(ids[0]).name}  #{Etc.getgrgid(ids[1]).name}  "
+def names(file_name)
+  ids = [File.stat(file_name).uid, File.stat(file_name).gid]
+  "#{Etc.getpwuid(ids[0]).name}  #{Etc.getgrgid(ids[1]).name}"
 end
 
-def print_number_of_links(content, nlink_counts)
-  biggest_number = nlink_counts.max_by(&:length).length
-  print "#{File.stat(content).nlink.to_s.rjust(biggest_number)} "
+def number_of_links(file_name, width_nlink)
+  File.stat(file_name).nlink.to_s.rjust(width_nlink)
 end
 
-def print_contents_size(content, file_size)
-  biggest_number = file_size.max_by(&:length).length
-  print "#{File.stat(content).size.to_s.rjust(biggest_number)} "
+def file_size(file_name, width_size)
+  File.stat(file_name).size.to_s.rjust(width_size)
 end
 
-def print_last_accessed_time(content)
-  print "#{File.stat(content).mtime.strftime('%_m %_d %H:%M')} "
+def last_accessed_time(file_name)
+  File.stat(file_name).mtime.strftime('%_m %_d %H:%M')
 end
 
 # シンボリックファイルに矢印をつける
-def print_content_names(content, lstat_mode_values, inode_values, lstat_mode)
-  current_lmode = lstat_mode
-  current_inode = File.stat(content).ino.to_s
-  matched_inode = inode_values.select { |_file, id| id == current_inode }
-  if current_lmode.start_with?('12')
-    matched_inode.each do |file, _id|
-      puts "#{content} -> #{file}" if !lstat_mode_values[file].start_with?('12')
-    end
+def files_and_symlinks(file_name)
+  lstat_mode = File.lstat(file_name).mode.to_s(8)
+  if lstat_mode.start_with?('12')
+    symbolic = File.readlink(file_name)
+    "#{file_name} -> #{symbolic}"
   else
-    puts content
+    file_name
   end
 end
 
