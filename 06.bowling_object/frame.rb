de354724a43730b325a6011ea562ce6241dcb33a
matsuyama <aya.k.matsuyama@gmail.com>
2024-02-19 01:58:40 +0900

    Frameクラスにpointsメソッドとstrike?メソッドを追加

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Frame
-  attr_reader :shots
-
   def initialize(shots, frame_number)
     @shots = shots
     @frame_number = frame_number
@@ -13,20 +11,28 @@ class Frame
     after_next_frame = frames[@frame_number + 2]
 
     if next_frame.nil?
-      current_frame_points_sum
+      points_sum
     else
-      current_frame_points_sum + bonus_points(next_frame, after_next_frame)
+      points_sum + bonus_points(next_frame, after_next_frame)
     end
+  end
+
+  def points
+    @shots.map(&:point)
+  end
+
+  def strike?
+    @shots[0].strike?
   end
 
   private
 
-  def current_frame_points_sum
+  def points_sum
     @shots.sum(&:point)
   end
 
   def bonus_points(next_frame, after_next_frame)
-    if @shots[0].strike?
+    if strike?
       strike_points(next_frame, after_next_frame)
     elsif spare?
       spare_points(next_frame)
@@ -36,19 +42,15 @@ class Frame
   end
 
   def strike_points(next_frame, after_next_frame)
-    next_frame_points = next_frame.shots.map(&:point)
-    after_next_frame_points = after_next_frame.shots.map(&:point) if after_next_frame
-
-    if next_frame.shots[0].strike? && after_next_frame
-      next_frame_points[0] + after_next_frame_points[0]
+    if next_frame.strike? && after_next_frame
+      next_frame.points[0] + after_next_frame.points[0]
     else
-      next_frame_points.take(2).sum
+      next_frame.points.take(2).sum
     end
   end
 
   def spare_points(next_frame)
-    next_frame_points = next_frame.shots.map(&:point)
-    next_frame_points[0]
+    next_frame.points[0]
   end
 
   def spare?
